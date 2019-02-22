4cb9f51e6027c3f4aac346bd8e512791e9842f26
NaraeKwon <nali4214@gmail.com>
2019-02-22 09:03:15 -0800

    Edited heading level.

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ createRestaurantHTML = (restaurant) => {
   image.tabIndex = '3';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.tabIndex = '3';
   li.append(name);
